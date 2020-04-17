110c491c304899b1e89f1225fbbc497fd402c3e8
Maria Rogozhkina <mariaspirit228@gmail.com>
2020-04-17 16:40:44 +0300

    Updates

---
+++ precache-manifest.de25a6d879182e4c2f25fdb3ab95285d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b02f69e59254f72debcf231a2a2097",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "7c2955b4f1243d06638a",
+    "url": "/my-app/static/css/main.6927a71f.chunk.css"
+  },
+  {
+    "revision": "c894e8a8e01d6a599ce8",
+    "url": "/my-app/static/js/2.07a05dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.07a05dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2955b4f1243d06638a",
+    "url": "/my-app/static/js/main.59c98576.chunk.js"
+  },
+  {
+    "revision": "7017a0a3783b2a9ce739",
+    "url": "/my-app/static/js/runtime-main.b51f6de3.js"
+  },
+  {
+    "revision": "f1f963a389d8c3d4becfb3dd2c74afec",
+    "url": "/my-app/static/media/car.f1f963a3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
